803c4378a72bb0d49822b66a1634fdc150231d75
Adam Brown <adamwbrown@gmail.com>
2024-11-24 17:51:28 -0800

    Implemented using Okio Much better

--- src/strategies/Fdict2Kmp.kt
+++ src/strategies/Fdict2Kmp.kt
@@ -1,60 +1,40 @@
 package strategies
 
-import korlibs.io.compression.compress
-import korlibs.io.compression.deflate.GZIP
-import korlibs.io.compression.uncompress
-import kotlinx.io.Buffer
-import kotlinx.io.buffered
-import kotlinx.io.files.Path
-import kotlinx.io.files.SystemFileSystem
-import kotlinx.io.readByteArray
-import utils.*
+import okio.FileSystem
+import okio.GzipSink
+import okio.GzipSource
+import okio.Path.Companion.toPath
+import okio.buffer
+import utils.readString
+import utils.readVariableLong
+import utils.writeString
+import utils.writeVariableLong
 
-val fdic2Path = Path("en-80k.fdic2")
+val fdic2Path = "en-80k.fdic2".toPath()
 
 fun writeFdic2Kmp(frequencyDict: Map<String, Long>) {
-    SystemFileSystem.delete(fdic2Path, mustExist = false)
+    // Delete existing file if it exists
+    FileSystem.SYSTEM.delete(fdic2Path, mustExist = false)
 
-    // Create a buffer to write to
-    val buffer = Buffer()
-
-    // Write to buffer
-    frequencyDict.forEach { (term, frequency) ->
-        buffer.writeVariableLength(frequency)
-        buffer.writeString(term)
-    }
-
-    val compressedBuffer = Buffer()
-    buffer.readByteArray().compress(GZIP).forEach { byte ->
-        compressedBuffer.writeByte(byte)
-    }
-
-    // Write to file
-    SystemFileSystem.sink(fdic2Path).use { sink ->
-        sink.write(compressedBuffer, compressedBuffer.size)
+    GzipSink(FileSystem.SYSTEM.sink(fdic2Path)).buffer().use { byteStream ->
+        frequencyDict.forEach { (term, frequency) ->
+            byteStream.writeVariableLong(frequency)
+            byteStream.writeString(term)
+        }
     }
 }
 
 fun readFdic2Kmp(): Map<String, Long> {
     val frequencyDict = mutableMapOf<String, Long>()
-    val buffer = CharArray(64)
 
-    // Read file into buffer
-    //val fieSize = SystemFileSystem.metadataOrNull(fdic2Path)!!.size
-    var fileBuffer: ByteArray
-    SystemFileSystem.source(fdic2Path).buffered().use { source ->
-        fileBuffer = source.readByteArray()
-    }
-
-    // Decompress
-    val decompressedBuffer = Buffer()
-    fileBuffer.uncompress(GZIP).forEach { byte -> decompressedBuffer.writeByte(byte) }
-
-    // Read dictionary entries
-    while (!decompressedBuffer.exhausted()) {
-        val frequency = decompressedBuffer.decodeVariableLengthLong()
-        val term = decompressedBuffer.readString(buffer)
-        frequencyDict[term] = frequency
+    // Read compressed file
+    GzipSource(FileSystem.SYSTEM.source(fdic2Path)).buffer().use { buffer ->
+        // Read dictionary entries
+        while (!buffer.exhausted()) {
+            val frequency = buffer.readVariableLong()
+            val term = buffer.readString()
+            frequencyDict[term] = frequency
+        }
     }
 
     return frequencyDict
--- src/utils/StreamUtilsKmp.kt
+++ src/utils/StreamUtilsKmp.kt
@@ -1,31 +1,20 @@
 package utils
 
-import kotlinx.io.Sink
-import kotlinx.io.Source
-import okio.GzipSource
+import okio.BufferedSink
+import okio.BufferedSource
 
-fun Sink.writeVariableLength(frequency: Long) = encodeVariableLengthLong(frequency, this)
-
-fun Sink.writeString(str: String) {
-    str.forEach { char ->
-        writeByte(char.code.toByte())
-    }
-    writeByte(0)
+fun BufferedSink.writeString(str: String) {
+    write(str.encodeToByteArray())
+    writeByte(0) // null terminator
 }
 
-fun Source.readString(buffer: CharArray = CharArray(64)): String {
-    var position = 0
-
+fun BufferedSource.readString(): String {
+    val bytes = mutableListOf<Byte>()
     while (true) {
-        val byte = readByte().toInt()
-        if (byte == 0 || byte == -1) break
-
-        if (position >= buffer.size) {
-            error("buffer of insufficient length")
-        }
-
-        buffer[position++] = byte.toChar()
+        if (exhausted()) break
+        val byte = readByte()
+        if (byte == 0.toByte()) break
+        bytes.add(byte)
     }
-
-    return String(buffer, 0, position)
+    return bytes.toByteArray().decodeToString()
 }
