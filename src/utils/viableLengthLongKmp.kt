9d2746927ef2e4180d3456d00952d0d68e38dadd
Adam Brown <adamwbrown@gmail.com>
2024-11-24 17:51:28 -0800

    Implemented using Okio Much better

--- src/utils/viableLengthLongKmp.kt
+++ src/utils/viableLengthLongKmp.kt
@@ -1,36 +1,27 @@
 package utils
 
-import kotlinx.io.Sink
-import kotlinx.io.Source
-import java.io.EOFException
-import java.io.InputStream
-import java.io.OutputStream
-import kotlin.experimental.or
+import okio.BufferedSink
+import okio.BufferedSource
 
-fun encodeVariableLengthLong(value: Long, sink: Sink) {
-    var remainingValue = value
+internal fun BufferedSink.writeVariableLong(value: Long) {
+    var v = value
     do {
-        var byte = (remainingValue and 0x7F).toByte()
-        remainingValue = remainingValue ushr 7
-        if (remainingValue != 0L) {
-            byte = byte or 0x80.toByte()
+        var b = (v and 0x7F).toByte()
+        v = v ushr 7
+        if (v != 0L) {
+            b = (b.toInt() or 0x80).toByte()
         }
-        sink.writeByte(byte)
-    } while (remainingValue != 0L)
+        writeByte(b.toInt())
+    } while (v != 0L)
 }
 
-fun Source.decodeVariableLengthLong(): Long {
+internal fun BufferedSource.readVariableLong(): Long {
     var value = 0L
     var shift = 0
-    var n = 0
     while (true) {
-        ++n
-        val byte = readByte().toInt()
-        val data = ((byte.toLong() and 0x7F) shl shift)
-        value = value or data
-        if (byte and 0x80 == 0) {
-            break
-        }
+        val b = readByte().toInt() and 0xFF
+        value = value or ((b.toLong() and 0x7F) shl shift)
+        if (b and 0x80 == 0) break
         shift += 7
     }
     return value
